5ca3ce2171fc65fac96c9c5e2d1c965fb5b75176
Kalu David <kaludavid411@gmail.com>
2024-10-18 11:43:11 +0100

    another commit

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = express()
 const PORT = 8000
 
 const rappers= {
-    '21 Savage': {      
+    '21 savage': {      
         'age': 29,
         'birthName': 'Sheyaa Bin Abraham-Joseph',
         'birthLocation': 'London, England'
     },
-    'Chance the Rapper': {      
+    'chance the rapper': {      
         'age': 29,
         'birthName': 'Chancelor Bennett',
         'birthLocation': 'Chicago, Illinois'
     },
-    'Unknown': {      
+    'unknown': {      
         'age': 0,
         'birthName': 'Unknown',
         'birthLocation': 'Unknown'
@@ -24,8 +24,13 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api', (req, res) => {
-    res.json(rappers)
+app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if(rappers[rapperName]){
+        res.json(rappers[rapperName])
+    }else{
+        res.json(rappers['unknown'])
+    }
 })
 
 app.listen(PORT, () => {
